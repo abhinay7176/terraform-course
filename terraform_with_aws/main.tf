a857ce8a0a88d2f6ed80c729404c4e84765b54eb
abhinay <abhinaysalure@gmail.com>
2023-02-24 06:30:00 +0000

    added terraform with aws

---
+++ terraform_with_aws/main.tf
@@ -0,0 +1,24 @@
+terraform {
+	required_providers {
+		aws = {
+		source = "hashicorp/aws"
+		version = "~> 4.16"
+		      }	
+	required_version = "~> 1.2.0"
+}
+          }
+
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+
+
+resource "aws_instance" "example" {
+	  ami  = "ami-0b828c1c5ac3f13ee"
+	  instance_type = "t2.micro"
+	tags = {
+		name = "terraform2"
+	}
+}
